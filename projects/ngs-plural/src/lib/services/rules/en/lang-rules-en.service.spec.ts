10890488bff296c9540340417e7eb0f2f67b5a5f
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2024-12-01 20:14:53 +0500

    update tests for English(en)

--- projects/ngs-plural/src/lib/services/rules/en/lang-rules-en.service.spec.ts
+++ projects/ngs-plural/src/lib/services/rules/en/lang-rules-en.service.spec.ts
@@ -10,18 +10,25 @@ describe('LangRulesEnService', () => {
     service = new LangRulesEnService();
   });
 
-  it('should return the plural form when count is 0', () => {
+  it('should return plural form for 0', () => {
     const result0 = service.applyRules(0, appleFormsEn);
     expect(result0).toBe(appleFormsEn[1]);
   });
 
-  it('should return the singular form when count is 1', () => {
+  it('should return singular form for 1', () => {
     const result1 = service.applyRules(1, appleFormsEn);
     expect(result1).toBe(appleFormsEn[0]);
   });
 
-  it('should return the plural form when count is 2', () => {
+  it('should return plural form for numbers greater than 1', () => {
     const result2 = service.applyRules(2, appleFormsEn);
+    const result5 = service.applyRules(5, appleFormsEn);
+    const result10 = service.applyRules(10, appleFormsEn);
+    const result100 = service.applyRules(100, appleFormsEn);
+
     expect(result2).toBe(appleFormsEn[1]);
+    expect(result5).toBe(appleFormsEn[1]);
+    expect(result10).toBe(appleFormsEn[1]);
+    expect(result100).toBe(appleFormsEn[1]);
   });
 });
