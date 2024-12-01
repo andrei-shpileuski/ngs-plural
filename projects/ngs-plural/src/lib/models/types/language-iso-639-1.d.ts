12b2e7e95bf7ed9608ea2830bd2e12f44636c19c
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2024-12-01 16:53:54 +0500

    + fix lang type

--- projects/ngs-plural/src/lib/models/types/language-iso-639-1.d.ts
+++ projects/ngs-plural/src/lib/models/types/language-iso-639-1.d.ts
@@ -1,3 +1,1 @@
-import { LanguageISO6391Enum } from '../enums/language-iso-639-1.enum';
-
-export type LanguageISO6391Type = `${LanguageISO6391Enum}`;
+export type LanguageISO6391Type = 'ru' | 'en';
