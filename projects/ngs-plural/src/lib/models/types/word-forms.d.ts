434336659d801bf36f3e92cbf354e200027cb89e
Andrei Shpileuski <shpilevskiy.aa@gmail.com>
2024-12-15 03:53:47 +0500

    fixed issues

--- projects/ngs-plural/src/lib/models/types/word-forms.d.ts
+++ projects/ngs-plural/src/lib/models/types/word-forms.d.ts
@@ -1,8 +1,1 @@
-import { FixedLengthArray } from './fixed-length-array';
-
-export type IWordForms<L extends number> = FixedLengthArray<string, L>;
-
-export type IWordFormsDefault = IWordForms<2>;
-export type IWordFormsSlavic = IWordForms<3>;
-
-export type PossibleWordForms = IWordFormsDefault | IWordFormsSlavic;
+export type PossibleWordForms = string[];
